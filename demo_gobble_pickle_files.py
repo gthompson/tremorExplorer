99e1da807464914e7e867f464e023babbadafe32
gthompson <glennthompson1971@gmail.com>
2023-10-04 18:08:33 -0400

    added demo gobbler and experimenting with sqlite3

--- demo_gobble_pickle_files.py
+++ demo_gobble_pickle_files.py
@@ -6,6 +6,7 @@ import FDSNtools
 import wrappers
 importlib.reload(wrappers)
 
+
 if sys.platform == 'linux':
     PRODUCTS_TOP = '/RAIDZ/IceWeb'
 elif sys.platform == 'darwin':
@@ -14,12 +15,6 @@ elif sys.platform == 'darwin':
 SDS_TOP=os.path.join(PRODUCTS_TOP,'SDS')
 if not os.path.isdir(SDS_TOP):
     os.makedirs(SDS_TOP)
-trace_ids = ['NZ.WIZ.10.HHZ', 'NZ.WSRZ.10.HHZ']
-centerlat=-37.52
-centerlon=177.1825
-searchRadiusDeg=0.25
-fdsnURL = "GEONET"
-subnet = 'Whakaari'
 SGRAM_TOP = os.path.join(PRODUCTS_TOP, 'sgram10min')
 if not os.path.isdir(SGRAM_TOP):
     os.makedirs(SGRAM_TOP)
@@ -27,9 +22,16 @@ PICKLEDIR = os.path.join(PRODUCTS_TOP, 'PICKLEFILE_QUEUE')
 if not os.path.isdir(PICKLEDIR):
     os.makedirs(PICKLEDIR)
 
-startt = UTCDateTime(2019,12,1)
+trace_ids = ['NZ.WIZ.10.HHZ', 'NZ.WSRZ.10.HHZ']
+centerlat=-37.52
+centerlon=177.1825
+searchRadiusDeg=0.25
+fdsnURL = "GEONET"
+subnet = 'Whakaari'
+
+startt = UTCDateTime(2019,1,1)
 endt = UTCDateTime(2019,12,12)
 inv = FDSNtools.get_inventory(fdsnURL, startt, endt, centerlat, centerlon, searchRadiusDeg, network='NZ', station='*', channel='HHZ')
-wrappers.SDS_to_picklefile_wrapper(startt, endt, SDS_TOP, freqmin=0.5, freqmax=20.0, \
-        zerophase=False, corners=2, sampling_interval=60.0, sourcelat=centerlat, \
-            sourcelon=centerlon, inv=inv, trace_ids=trace_ids, overwrite=False, verbose=True, timeWindowMinutes=10,  timeWindowOverlapMinutes=5, PICKLEDIR=PICKLEDIR, subnet=subnet)
+wrappers.picklefileGobblerToIceweb(PICKLEDIR, verbose=True, rsamSamplingIntervalSeconds=60, RSAM_SDS_TOP=SDS_TOP, SGRAM_TOP=SGRAM_TOP, dbscale=True, \
+                              equal_scale=True, clim=[1e-8,1e-5], fmin=0.5, fmax=20.0, overwrite=False)
+
--- lib/wrappers.py
+++ lib/wrappers.py
@@ -1,11 +1,12 @@
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-from obspy.core import Stream
+from obspy.core import Stream, read
 import SDS
 import FDSNtools
 import InventoryTools
 import IceWeb
+import gc
 
 def FDSN_to_SDS_daily_wrapper(startt, endt, SDS_TOP, centerlat=None, centerlon=None, searchRadiusDeg=None, trace_ids=None, \
         fdsnURL="http://service.iris.edu", overwrite=True, inv=None):
@@ -373,7 +374,7 @@ def SDS_to_picklefile_wrapper(startt, endt, SDS_TOP, freqmin=0.5, freqmax=None, 
     taperSecs = timeWindowOverlapMinutes * 60
     startOfTimeWindow = startt
     while startOfTimeWindow < endt:
-        f"Processing {startOfTimeWindow}"
+        print(f"Processing {startOfTimeWindow}")
         endOfTimeWindow = startOfTimeWindow + timeWindowMinutes * 60
         # read from SDS
         thisSDSobj = SDS.SDSobj(SDS_TOP) 
@@ -387,8 +388,8 @@ def SDS_to_picklefile_wrapper(startt, endt, SDS_TOP, freqmin=0.5, freqmax=None, 
             InventoryTools.attach_distance_to_stream(st, sourcelat, sourcelon) 
             r = [tr.stats.distance for tr in st]
             if verbose:
-                f"SDS Stream: {st}"
-                f"Distances: {r}"
+                print(f"SDS Stream: {st}")
+                print(f"Distances: {r}")
             st = order_traces_by_distance(st, r, assert_channel_order=True)
             print(st, [tr.stats.distance for tr in st])
             #VEL = order_traces_by_distance(VEL, r, assert_channel_order=True)
@@ -396,10 +397,10 @@ def SDS_to_picklefile_wrapper(startt, endt, SDS_TOP, freqmin=0.5, freqmax=None, 
             pre_filt = [freqmin/1.2, freqmin, freqmax, freqmax*1.2]
             for seismogramType in ['VEL', 'DISP']:
                 if verbose:
-                    f"Correcting to {seismogramType} seismogram"
+                    print(f"Correcting to {seismogramType} seismogram")
                 cst = st.copy().select(channel="*H*").remove_response(output=seismogramType, inventory=inv, plot=verbose, pre_filt=pre_filt, water_level=60)
                 if verbose:
-                    f"Trimming to 24-hour day from {startOfTimeWindow} to {endOfTimeWindow}"
+                    print(f"Trimming to 24-hour day from {startOfTimeWindow} to {endOfTimeWindow}")
                 cst.trim(starttime=startOfTimeWindow, endtime=endOfTimeWindow)
                 picklebase = '%s_%s_%s.pickle' % (subnet, startOfTimeWindow.strftime('%Y%m%d-%H%M'), seismogramType)
                 picklefile = os.path.join(PICKLEDIR, picklebase)
@@ -408,6 +409,7 @@ def SDS_to_picklefile_wrapper(startt, endt, SDS_TOP, freqmin=0.5, freqmax=None, 
                 del cst, picklefile, picklebase
 
             del st, r, pre_filt
+        gc.collect()
         startOfTimeWindow = endOfTimeWindow
 
 def sorted_ls(path):
@@ -415,7 +417,7 @@ def sorted_ls(path):
     return list(sorted(os.listdir(path), key=mtime))    
 
 def picklefileGobblerToIceweb(PICKLEDIR, verbose=False, rsamSamplingIntervalSeconds=60, RSAM_SDS_TOP='.', SGRAM_TOP='.', dbscale=True, \
-                              equal_scale=True, clim=[1e-8,1e-5], fmin=0.5, fmax=None, overwrite=False):
+                              equal_scale=True, clim=[1e-8,1e-5], fmin=0.5, fmax=None, overwrite=False, subnet='unknown'):
     '''
     Gobble up any picklefiles found and process them into IceWeb products.
 
@@ -427,18 +429,26 @@ def picklefileGobblerToIceweb(PICKLEDIR, verbose=False, rsamSamplingIntervalSeco
     running at UAF 2009-2013 (I only noticed it stopped running in 2023).
 
     '''
-
+ 
     while True:
-          picklefilelist = sorted_ls(PICKLE_DIR) # make list of picklefiles
-          if len(filelist)>0:
-            picklefile = filelist[0]
-            f"Processing {picklefile}"
+          counter=0
+          picklefilelist = sorted_ls(PICKLEDIR) # make list of picklefiles
+          #print(f"{picklefilelist}")
+          if len(picklefilelist)>0:
+            counter=0
+            picklefile = os.path.join(PICKLEDIR, picklefilelist[0])
+            print(f"Processing {picklefile}")
             if '.pickle' in picklefile:
-                st = obspy.read(picklefile, format='PICKLE')
+                try:
+                    st = read(picklefile, format='PICKLE')
+                except Exception as e:
+                    print(f"{e}")
+                    continue
                 if isinstance(st, Stream) and len(st)>0 and st[0].stats.npts>1000:
                     pass
                 else:
-                    f"Not a valid Stream object: {st}"
+                    print(f"Not a valid Stream object: {st}")
+                    del st
                     os.remove(picklefile) # delete file
                     continue # next while loop iteration
 
@@ -448,10 +458,10 @@ def picklefileGobblerToIceweb(PICKLEDIR, verbose=False, rsamSamplingIntervalSeco
                     
                     # compute & save instrument-corrected RSAM
                     if verbose:
-                        f"Computing corrected RSAM"
-                    thisRSAMobj = IceWeb.RSAMobj(st=VEL, sampling_interval=rsamSamplingIntervalSeconds, verbose=verbose,  units='m/s', absolute=True)
+                        print(f"Computing corrected RSAM")
+                    thisRSAMobj = IceWeb.RSAMobj(st=st, sampling_interval=rsamSamplingIntervalSeconds, verbose=verbose,  units='m/s', absolute=True)
                     if verbose:
-                        f"Saving corrected RSAM to SDS"
+                        print(f"Saving corrected RSAM to SDS")
                     thisRSAMobj.write(RSAM_SDS_TOP) # write RSAM to an SDS-like structure
                     del thisRSAMobj
 
@@ -464,10 +474,10 @@ def picklefileGobblerToIceweb(PICKLEDIR, verbose=False, rsamSamplingIntervalSeco
                     if not os.path.isdir(sgramdir):
                         os.makedirs(sgramdir)
                     if not os.path.isfile(sgramfile) or overwrite:
-                        f"Output file: {sgramfile}"
+                        print(f"Output file: {sgramfile}")
                         spobj = IceWeb.icewebSpectrogram(stream=st)
-                        spobj.plot(outfile=sgramfile, dbscale=dbscale, title=sgramfile, equal_scale=equal_scale, clim=clim, fmin=freqmin, fmax=freqmax)
-                        del sbobj
+                        spobj.plot(outfile=sgramfile, dbscale=dbscale, title=sgramfile, equal_scale=equal_scale, clim=clim, fmin=fmin, fmax=fmax)
+                        del spobj
                     plt.close('all')
 
                     del startt, sgramdir, sgrambase, sgramfile
@@ -476,12 +486,147 @@ def picklefileGobblerToIceweb(PICKLEDIR, verbose=False, rsamSamplingIntervalSeco
                 elif 'DISP' in picklefile:
                      # compute/write reduced displacement
                     if verbose:
-                        f"Computing DRS"
-                    thisDRSobj = IceWeb.ReducedDisplacementObj(st=DISP, sampling_interval=rsamSamplingIntervalSeconds, verbose=verbose, units='m' )
+                        print(f"Computing DRS")
+                    thisDRSobj = IceWeb.ReducedDisplacementObj(st=st, sampling_interval=rsamSamplingIntervalSeconds, verbose=verbose, units='m' )
                     if verbose:
-                        f"Writing DRS to SDS"
+                        print(f"Writing DRS to SDS")
                     thisDRSobj.write(RSAM_SDS_TOP) # write Drs to an SDS-like structure
                     del thisDRSobj
+                del st
                 
                 # delete file
                 os.remove(picklefile)
+
+                gc.collect()
+
+          else:
+              if counter==0:
+              	f"Waiting for new pickle file"  
+              else:
+                print('.', end='')
+                
+              counter += 1    
+
+
+import sqlite3
+from sqlite3 import Error
+
+def create_connection(db_file):
+    """ create a database connection to a SQLite database """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        print(sqlite3.version)
+    except Error as e:
+        print(e)
+    finally:
+        return conn
+
+
+def create_table(conn, create_table_sql):
+    """ create a table from the create_table_sql statement
+    :param conn: Connection object
+    :param create_table_sql: a CREATE TABLE statement
+    :return:
+    """
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print(e)
+
+
+def create_iceweb_index_db(dbpath):
+
+    sql_create_index_table = """ CREATE TABLE IF NOT EXISTS icewebindex (
+                                        subnet text NOT NULL,
+                                        startTime integer NOT NULL,
+                                        endTime integer,
+                                        rsamDone integer,
+                                        drsDone integer,
+                                        sgramDone integer,
+                                        PRIMARY KEY (subnet, startTime, endTime)
+                                    ); """
+
+
+    # create a database connection
+    conn = create_connection(dbpath)
+    print(conn)
+
+    # create tables
+    if conn is not None:
+        # create index table
+        create_table(conn, sql_create_index_table)
+    else:
+        print("Error! cannot create the database connection.")
+        create_table(conn, sql_create_index_table)
+    return conn
+
+def insert_index_row(conn, indexrow):
+    """
+    Create a new index row into the index table
+    :param conn:
+    :param indexrow
+    :return: did it work (True, False)
+    """
+    sql = ''' INSERT INTO icewebindex(subnet, startTime, endTime, rsamDone, drsDone, sgramDone)
+              VALUES(?,?,?,?,?,?) '''
+    cur = conn.cursor()
+    try:
+        cur.execute(sql, indexrow)
+        conn.commit()
+        return True
+    except Exception as e:
+        print(e)
+        return False
+
+def select_indexrow_by_primary_key(conn, subnet, startTime):
+    """
+    Query tasks by subnet, startTime
+    :param conn: the Connection object
+    :param subnet:
+    :param startTime:
+    :return:
+
+    Can use this to check if rsamDone, drsDone, sgramDone, etc.
+    """
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM icewebindex WHERE subnet=? AND startTime=?", (subnet,startTime))
+
+    rows = cur.fetchall()
+    if len(rows)==1:
+        print(rows[0])
+        return rows[0] # a tuple
+
+
+def update_fieldDone(conn, subnet, startTime, field='rsamDone', value=True):
+    """
+    Query tasks by subnet, startTime
+    :param conn: the Connection object
+    :param subnet:
+    :param startTime:
+    :return:
+    """
+    cur = conn.cursor()
+    cur.execute(f"UPDATE icewebindex set {field}=? WHERE subnet=? AND startTime=?", (value, subnet, startTime))
+    conn.commit()
+
+
+
+
+if __name__ == '__main__':
+    dbpath = '/RAIDZ/IceWeb/iceweb_sqllite3.db'
+    conn = create_iceweb_index_db(dbpath)
+    subnet = 'Shishaldin'
+    stime = 1696454518
+    etime = stime + 600
+    if conn is not None:
+        indexrow = ('Shishaldin', stime, etime, False, False, False)
+        diditwork = insert_index_row(conn, indexrow) # diditwork False if unique row already exists
+        matchingrow = select_indexrow_by_primary_key(conn, subnet, stime)
+        update_fieldDone(conn, subnet, stime, field='rsamDone')
+        matchingrow = select_indexrow_by_primary_key(conn, subnet, stime)
+        update_fieldDone(conn, subnet, stime, field='sgramDone')
+        matchingrow = select_indexrow_by_primary_key(conn, subnet, stime)
+        conn.close()
+
